0aae5f57c530539373ae343b82737021f93d1965
sculouis <scu.louis@gmail.com>
2021-08-12 15:02:20 +0800

    add viewcomponent

--- TestSelect2/Controllers/HomeController.cs
+++ TestSelect2/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace TestSelect2.Controllers
             return View();
         }
 
+        public IActionResult Viewcomponent()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ TestSelect2/ViewComponents/TestViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestSelect2.ViewComponents
+{
+    public class TestViewComponent: ViewComponent
+    {
+        public TestViewComponent()
+        {
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(
+        int maxPriority, bool isDone)
+        {
+            return View();
+        }
+
+    }
+}
